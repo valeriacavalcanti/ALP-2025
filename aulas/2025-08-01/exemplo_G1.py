f39770efab245c1f65002a422ceeaefcbfa00791
Valeria Cavalcanti <valeria.cavalcanti@ifpb.edu.br>
2025-08-29 10:28:08 -0300

    Aula 01/08/2025

---
+++ aulas/2025-08-01/exemplo_G1.py
@@ -0,0 +1,9 @@
+numero1 = input('Informe um valor: ')
+numero2 = input('Informe outro valor: ')
+
+numero1 = int(numero1)
+numero2 = int(numero2)
+
+soma = numero1 + numero2
+
+print(soma)
---
+++ aulas/2025-08-01/exemplo_G2.py
@@ -0,0 +1,9 @@
+numero1 = input('Digite o primeiro número: ')
+numero2 = input('Digite o segundo número: ')
+
+numero1 = int(numero1)
+numero2 = int(numero2)
+
+soma = numero1 + numero2
+
+print(soma)
